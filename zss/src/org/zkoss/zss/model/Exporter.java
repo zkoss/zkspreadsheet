e8bed9973e06db5b735c9652974c1ae5dc0c24b5
Henri Chen <henrichen@rikulo.org>
2010-09-06 10:38:45 +0000

    Fine tune the file/method comments.

--- zss/src/org/zkoss/zss/model/Exporter.java
+++ zss/src/org/zkoss/zss/model/Exporter.java
@@ -12,13 +12,10 @@ Copyright (C) 2010 Potix Corporation. All Rights Reserved.
 package org.zkoss.zss.model;
 
 import java.io.OutputStream;
-import org.apache.poi.ss.usermodel.Workbook;
 import org.zkoss.zss.model.Book;
 
-import com.itextpdf.text.DocumentException;
-
 /**
- * This interface defines methods for converting POI excel data model {@link Workbook} into another 
+ * This interface defines methods for exporting ZK Spreadsheet {@link Book} into another 
  * format written to a  {@link OutputStream}.
  * @author ashish
  *
@@ -26,7 +23,7 @@ import com.itextpdf.text.DocumentException;
 public interface Exporter {
 
 	/**
-	 * Converts Apache POI excel data model {@link Workbook} into another format written to a 
+	 * Exports ZK Spreadsheet {@link Book} into another format written to a 
 	 * {@link OutputStream}.
 	 * @param workbook
 	 * @param outputStream
