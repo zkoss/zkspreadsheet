613ba27e521498acf03e5d19ccbf3c1fad53dc86
Henri Chen <henrichen@rikulo.org>
2010-09-24 15:36:30 +0000

    bug 97 StringIndexOutOfBoundsException if cell text format is _(*"_"??_)

---
+++ zsstest/src/org/zkoss/zss/model/impl/Book15XlsTextFormatTest.java
@@ -0,0 +1,93 @@
+/**
+ * 
+ */
+package org.zkoss.zss.model.impl;
+
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+import java.io.InputStream;
+import java.util.Set;
+
+import org.apache.poi.hssf.record.constant.ErrorConstant;
+import org.apache.poi.hssf.record.formula.Ptg;
+import org.apache.poi.hssf.usermodel.HSSFEvaluationWorkbook;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.formula.FormulaRenderer;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellValue;
+import org.apache.poi.ss.usermodel.ErrorConstants;
+import org.apache.poi.ss.usermodel.FormulaEvaluator;
+import org.apache.poi.ss.usermodel.Hyperlink;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.util.CellRangeAddress;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.zkoss.util.resource.ClassLocator;
+import org.zkoss.zss.engine.Ref;
+import org.zkoss.zss.engine.impl.CellRefImpl;
+import org.zkoss.zss.model.Book;
+import org.zkoss.zss.model.FormatText;
+import org.zkoss.zss.model.impl.BookHelper;
+import org.zkoss.zss.model.impl.ExcelImporter;
+import org.zkoss.zss.model.impl.HSSFBookImpl;
+import org.zkoss.zss.ui.impl.Utils;
+
+/**
+ * Check if the Text format correctly.
+ * @author henrichen
+ */
+public class Book15XlsTextFormatTest {
+	private Workbook _workbook;
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+		final String filename = "Book15.xls";
+		final InputStream is = new ClassLocator().getResourceAsStream(filename);
+		_workbook = new ExcelImporter().imports(is, filename);
+		assertTrue(_workbook instanceof Book);
+		assertTrue(_workbook instanceof HSSFBookImpl);
+		assertTrue(_workbook instanceof HSSFWorkbook);
+		assertEquals(filename, ((Book)_workbook).getBookName());
+		assertEquals("Sheet1", _workbook.getSheetName(0));
+		assertEquals("Sheet2", _workbook.getSheetName(1));
+		assertEquals("Sheet3", _workbook.getSheetName(2));
+		assertEquals(0, _workbook.getSheetIndex("Sheet1"));
+		assertEquals(1, _workbook.getSheetIndex("Sheet2"));
+		assertEquals(2, _workbook.getSheetIndex("Sheet3"));
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+		_workbook = null;
+	}
+	
+	@Test
+	public void testCellNumberFormatter() {
+		Sheet sheet1 = _workbook.getSheet("Sheet1");
+		Row row2 = sheet1.getRow(1);
+	
+		testCell(row2, 0, " -   "); //1 space - 3 spaces
+	}
+
+	private void testCell(Row row, int colindex, String expect) {
+		Cell cell = row.getCell(colindex);
+		FormatText c = BookHelper.getFormatText(cell);
+		assertTrue(!c.isRichTextString());
+		assertTrue(c.isCellFormatResult());
+		
+		assertEquals(expect, c.getCellFormatResult().text);
+	}
+
+}
