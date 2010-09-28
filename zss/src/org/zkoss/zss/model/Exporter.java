fb03197c5ba6f54f039ac8bdcb0ee6099995a45a
Ashish <ashish.dasnurkar@gmail.com>
2010-09-28 07:36:56 +0000

    Updated Exporter interface for feature 111 Support exporting selected sheet to pdf and feature 112 Support exporting selected area to pdf

--- zss/src/org/zkoss/zss/model/Exporter.java
+++ zss/src/org/zkoss/zss/model/Exporter.java
@@ -12,6 +12,9 @@ Copyright (C) 2010 Potix Corporation. All Rights Reserved.
 package org.zkoss.zss.model;
 
 import java.io.OutputStream;
+
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.util.AreaReference;
 import org.zkoss.zss.model.Book;
 
 /**
@@ -24,10 +27,25 @@ public interface Exporter {
 
 	/**
 	 * Exports ZK Spreadsheet {@link Book} into another format written to a 
-	 * {@link OutputStream}.
+	 * {@link OutputStream}. Note that it exports entire workbook. 
 	 * @param workbook
 	 * @param outputStream
-	 * @throws DocumentException 
 	 */
 	public void export(Book workbook, OutputStream outputStream);
+
+	/**
+	 * Exports ZK Spreadsheet sheet into another format written to a 
+	 * @param worksheet sheet instance that contains selected area
+	 * @param outputStream outoutStream to which exported contents to be written
+	 */
+	public void export(Sheet worksheet, OutputStream outputStream);
+
+	/**
+	 * Exports selected area of ZK Spreadsheet active sheet represented by 
+	 * {@link org.zkoss.zss.model.Range}
+	 * @param worksheet sheet instance that contains selected area
+	 * @param area area representing selected area to be exported
+	 * @param outputStream outoutStream to which exported contents to be written
+	 */
+	public void exportSelection(Sheet worksheet, AreaReference area, OutputStream outputStream);
 }
