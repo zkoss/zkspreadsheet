6fde92e230690e4dd0cebc97346bb2131ec85ea2
whkuoattaiwan <whkuoattaiwan@potix.com>
2010-12-16 09:20:13 +0000

    add peter's test cases

--- zssapp/test/SS_131_Test.java
+++ zssapp/test/SS_131_Test.java
@@ -17,14 +17,27 @@ it will be useful, but WITHOUT ANY WARRANTY.
 public class SS_131_Test extends SSAbstractTestCase {
 	@Override
 	protected void executeTest() {
+		//verify
+		String oriF12value = getSpecifiedCell(5,11).text();
+		String oriF13value = getSpecifiedCell(5,12).text();		
+		verifyNotEquals(oriF12value, oriF13value);
+		String oriG12value = getSpecifiedCell(6,11).text();
+		String oriG13value = getSpecifiedCell(6,12).text();		
+		verifyNotEquals(oriG12value, oriG13value);
+
+		
 		rightClickCell(5,11);
 		mouseOver(jq("a.z-menu-cnt:eq(1)"));		
 		waitResponse();
 		click(jq("$deleteEntireRow a.z-menu-item-cnt"));
 		waitResponse();
 		
-		//how to verify
-		sleep(5000);
+		//verify
+		String f12value = getSpecifiedCell(5,11).text();
+		verifyEquals(f12value, oriF13value);
+		String g12value = getSpecifiedCell(6,11).text();
+		verifyEquals(g12value, oriG13value);
+		
 	}
 }
 
--- zssapp/test/SS_132_Test.java
+++ zssapp/test/SS_132_Test.java
@@ -17,14 +17,27 @@ it will be useful, but WITHOUT ANY WARRANTY.
 public class SS_132_Test extends SSAbstractTestCase {
 	@Override
 	protected void executeTest() {
+		//verify
+		String oriF12value = getSpecifiedCell(5,11).text();
+		String oriG12value = getSpecifiedCell(6,11).text();		
+		verifyNotEquals(oriF12value, oriG12value);
+		String oriF13value = getSpecifiedCell(5,12).text();
+		String oriG13value = getSpecifiedCell(6,12).text();		
+		verifyNotEquals(oriF13value, oriG13value);
+		
+		
 		rightClickCell(5,11);
 		mouseOver(jq("a.z-menu-cnt:eq(1)"));		
 		waitResponse();
 		click(jq("$deleteEntireColumn a.z-menu-item-cnt"));
 		waitResponse();
 		
-		//how to verify
-		sleep(5000);
+		//verify
+		String f12value = getSpecifiedCell(5,11).text();
+		verifyEquals(f12value, oriG12value);
+		String f13value = getSpecifiedCell(5,12).text();
+		verifyEquals(f13value, oriG13value);
+		
 	}
 }
 
