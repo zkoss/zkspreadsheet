d032bf167474694b4a72ba842682b971c0533ec9
whkuoattaiwan <whkuoattaiwan@potix.com>
2010-12-16 08:06:46 +0000

    add peter's test cases

--- zssapp/test/SS_123_Test.java
+++ zssapp/test/SS_123_Test.java
@@ -17,15 +17,28 @@ it will be useful, but WITHOUT ANY WARRANTY.
 public class SS_123_Test extends SSAbstractTestCase {
 	@Override
 	protected void executeTest() {
+		//verify
+		String f12value = getSpecifiedCell(5,11).text();
+		String g12value = getSpecifiedCell(6,11).text();
+		verifyNotEquals(f12value, g12value);
+		verifyTrue(jq("div.zshighlight").width() == 0);
+		
+		
 		rightClickCell(5,11);
 		click(jq("$copy a.z-menu-item-cnt"));
 		waitResponse();
 
+		//verify
+		verifyTrue(jq("div.zshighlight").width() != 0);
+
 		rightClickCell(6,11);
 		click(jq("$paste a.z-menu-item-cnt"));
 		waitResponse();
-		//how to verify
-		sleep(5000);
+
+		//verify
+		f12value = getSpecifiedCell(5,11).text();
+		g12value = getSpecifiedCell(6,11).text();
+		verifyEquals(f12value, g12value);
 	}
 }
 
