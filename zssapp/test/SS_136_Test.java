74e6fd7258b8bfedcd9b0c22b8f79bd84a693625
whkuoattaiwan <whkuoattaiwan@potix.com>
2010-12-17 07:36:20 +0000

    add peter's test cases

--- zssapp/test/SS_136_Test.java
+++ zssapp/test/SS_136_Test.java
@@ -13,24 +13,24 @@ This program is distributed under Apache License Version 2.0 in the hope that
 it will be useful, but WITHOUT ANY WARRANTY.
 */
 
-//sort ==> Ascending B13~B17
-public class SS_135_Test extends SSAbstractTestCase {
+//sort ==> Descending B13~B17
+public class SS_136_Test extends SSAbstractTestCase {
 	@Override
 	protected void executeTest() {
 		rightClickCells(1,12,1,16);
 	
 		mouseOver(jq("a.z-menu-cnt:eq(3)"));		
 		waitResponse();
-		click(jq("$sortAscending a.z-menu-item-cnt"));
+		click(jq("$sortDescending a.z-menu-item-cnt"));
 		waitResponse();
 		
 		//verify
 		String b13text = getSpecifiedCell(1, 12).text();
-		verifyEquals(b13text, "Average total assets");
+		verifyEquals(b13text, "Total assets");
 		String b15text = getSpecifiedCell(1, 14).text();
 		verifyEquals(b15text, "Current assets");
 		String b17text = getSpecifiedCell(1, 16).text();
-		verifyEquals(b17text, "Total assets");
+		verifyEquals(b17text, "Average total assets");
 	}
 }
 
