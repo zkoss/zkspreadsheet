31593b64695c71054378f9e583b6d6ad27edb12e
whkuoattaiwan <whkuoattaiwan@potix.com>
2010-12-17 08:41:42 +0000

    add peter's test cases

---
+++ zssapp/test/SS_148_Test.java
@@ -0,0 +1,47 @@
+import org.zkoss.ztl.JQuery;
+import org.zkoss.ztl.util.ColorVerifingHelper;
+
+/* order_test_1Test.java
+
+	Purpose:
+		
+	Description:
+		
+	History:
+		Sep, 7, 2010 17:30:59 PM
+
+Copyright (C) 2010 Potix Corporation. All Rights Reserved.
+
+This program is distributed under Apache License Version 2.0 in the hope that
+it will be useful, but WITHOUT ANY WARRANTY.
+*/
+
+//right click "font color" : B13
+public class SS_148_Test extends SSAbstractTestCase {
+	@Override
+	protected void executeTest() {
+		rightClickCell(1,12);
+		click(jq("div[title=\"Font color\"] img.z-colorbtn-btn:eq(2)"));
+		waitResponse();
+
+	    // Input color hex code, then press Enter.
+        JQuery colorTextbox = jq(".z-colorpalette-hex-inp:eq(5)");
+        String fontColorStr = "#00ff00";
+        type(colorTextbox, fontColorStr);
+        keyPressEnter(colorTextbox);
+        
+        //Verify
+        JQuery cell_B_13 = getSpecifiedCell(1, 12);
+        String style = cell_B_13.css("color");
+
+        //input "#00ff00", but it actually get "009900"
+        //Is it acceptable in this spec?
+        if (style != null) {
+            verifyTrue("Unexcepted result: " + cell_B_13.css("color"), 
+            		ColorVerifingHelper.isEqualColor("#009900", style));
+        } else {
+            verifyTrue("Cannot get style of specified cell!", false);
+        }		
+	}
+}
+
