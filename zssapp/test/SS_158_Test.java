ee06191452bcf21b1ac011245cdacf63d2355914
whkuoattaiwan <whkuoattaiwan@potix.com>
2010-12-20 02:20:06 +0000

    add peter's test cases

--- zssapp/test/SS_158_Test.java
+++ zssapp/test/SS_158_Test.java
@@ -18,14 +18,26 @@ public class SS_158_Test extends SSAbstractTestCase {
 	@Override
 	protected void executeTest() {
 		//verify
-		verifyTrue(jq("div.zshighlight").width() == 0);
+		String beforeF6Style = getCellCompositeStyle(5, 5);
 
 		selectCells(5,5,8,8);
 
 		//D:68
 		pressCtrlWithChar("68");
 		
-		//how to verify
+		//verify
+		String afterF6Style = getCellCompositeStyle(5, 5);
+		String f6value = getSpecifiedCell(5,5).text();
+		verifyEquals(f6value,null);
+
+		String f7value = getSpecifiedCell(5,6).text();
+		verifyEquals(f7value,null);
+		
+		String g8value = getSpecifiedCell(6,7).text();
+		verifyEquals(g8value,null);
+		
+		verifyEquals(beforeF6Style,afterF6Style);
+
 	}
 }
 
