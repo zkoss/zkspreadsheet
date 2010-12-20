b3cee598425a3991bf986d4c2fd357e1bcdc41ae
Henri Chen <henrichen@rikulo.org>
2010-12-20 08:01:00 +0000

    1. fine tune ZK Spreadsheet Live: tab on formula editbox shall take care merged cell when move focus to the right. 2. bug 248: ZK Spreadsheet Live: Tab on formula editor if focus on the last visible column, shall not move focus right 3. bug 249: ZK Spreadsheet Live: Enter on formula editor if focus on the last visible row, shall not move focus down

--- zssapp/src/org/zkoss/zss/app/zul/FormulaEditor.java
+++ zssapp/src/org/zkoss/zss/app/zul/FormulaEditor.java
@@ -15,6 +15,7 @@ Copyright (C) 2009 Potix Corporation. All Rights Reserved.
 package org.zkoss.zss.app.zul;
 
 import org.zkoss.poi.ss.usermodel.Cell;
+import org.zkoss.poi.ss.util.CellRangeAddress;
 import org.zkoss.zss.model.Worksheet;
 import org.zkoss.zk.ui.Executions;
 import org.zkoss.zk.ui.event.Event;
@@ -23,6 +24,7 @@ import org.zkoss.zk.ui.event.InputEvent;
 import org.zkoss.zss.app.cell.EditHelper;
 import org.zkoss.zss.app.zul.ctrl.DesktopWorkbenchContext;
 import org.zkoss.zss.model.Ranges;
+import org.zkoss.zss.model.impl.SheetCtrl;
 import org.zkoss.zss.ui.Position;
 import org.zkoss.zss.ui.Spreadsheet;
 import org.zkoss.zss.ui.event.CellEvent;
@@ -96,8 +98,13 @@ public class FormulaEditor extends Textbox implements ZssappComponent{
 				getText());
 		Position pos = ss.getCellFocus();
 		int row = pos.getRow();
-		int col = pos.getColumn() + 1;
-
+		int col = pos.getColumn();
+		final Worksheet sheet = ss.getSelectedSheet();
+		final CellRangeAddress merged = sheet != null ? ((SheetCtrl)sheet).getMerged(row, col) : null;
+		col = merged == null ? col + 1 : merged.getLastColumn() + 1;
+		if (ss.getMaxcolumns() <= col) {
+			col = ss.getMaxcolumns() - 1;
+		}
 		ss.focusTo(row, col);
 		getDesktopWorkbenchContext().getWorkbookCtrl().reGainFocus();
 	}
@@ -111,7 +118,9 @@ public class FormulaEditor extends Textbox implements ZssappComponent{
 		Position pos = ss.getCellFocus();
 		int row = pos.getRow() + 1;
 		int col = pos.getColumn();
-
+		if (ss.getMaxrows() <= row) {
+			row = ss.getMaxrows() - 1;
+		}
 		ss.focusTo(row, col);
 		getDesktopWorkbenchContext().getWorkbookCtrl().reGainFocus();
 	}
