1fecce3a9a009ce709713bf6260f2f5e2230e6b4
Sam Chuang <qk168899@gmail.com>
2010-12-22 04:09:22 +0000

    fine-tune test case

--- zssapp/test/SS_041_Test.java
+++ zssapp/test/SS_041_Test.java
@@ -9,7 +9,7 @@ public class SS_041_Test extends SSAbstractTestCase {
     protected void executeTest() {
 //    	int origChildren = widget(jq("@tab")).nChildren();
     	JQuery marketTab = jq("@tab[label=\"Market\"] span.z-tab-text");
-    	int origChildren = jq("@tab").children().length();
+    	int origChildren = jq("$sheets @tab").children().length();
     	click("jq('$insertMenu button.z-menu-btn')");
     	waitResponse();
     	click("jq('$insertSheet a.z-menu-item-cnt')");
