3027e14bd87292d5babddf3e1fa9bf4fbf14264b
Sam Chuang <qk168899@gmail.com>
2010-12-22 08:49:24 +0000

    ztl fine-tune code

--- zssapp/test/SS_083_Test.java
+++ zssapp/test/SS_083_Test.java
@@ -5,22 +5,22 @@ public class SS_083_Test extends SSAbstractTestCase {
 
     @Override
     protected void executeTest() {
-        JQuery cell_B_10 = loadCell();
-        int originalWidth = cell_B_10.width();
-        clickCell(cell_B_10);
-        clickCell(cell_B_10);
+        JQuery cell_B_9 = loadCell();
+        int originalWidth = cell_B_9.width();
+        clickCell(cell_B_9);
+        clickCell(cell_B_9);
         click(jq("$wrapTextBtn"));
         waitResponse();
         
-        cell_B_10 = loadCell();
-        int currentWidth = cell_B_10.width();        
+        cell_B_9 = loadCell();
+        int currentWidth = cell_B_9.width();
         verifyTrue("Unexcepted result: " + currentWidth, currentWidth < originalWidth);
         
         sleep(8000);
     }
 
     private JQuery loadCell() {
-        return getSpecifiedCell(1, 9);
+        return getSpecifiedCell(1, 8);
     }
 
 }
