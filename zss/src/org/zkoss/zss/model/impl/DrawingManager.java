2ad11f93621d224a7086833a7152ca9eec021547
Henri Chen <henrichen@rikulo.org>
2011-01-13 07:06:42 +0000

    Add new DrawingManager interface

---
+++ zss/src/org/zkoss/zss/model/impl/DrawingManager.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package org.zkoss.zss.model.impl;
+
+import java.util.List;
+
+import org.zkoss.poi.ss.usermodel.Chart;
+import org.zkoss.poi.ss.usermodel.Picture;
+
+/**
+ * ZK Spreadsheet Drawing manager. 
+ * @author henrichen
+ *
+ */
+public interface DrawingManager {
+	/**
+	 * Return pictures in sheet.
+	 * @return pictures in sheet.
+	 */
+	public List<Picture> getPictures();
+	
+	/**
+	 * Returns charts in sheet.
+	 * @return charts in sheet.
+	 */
+	public List<Chart> getCharts();
+}
