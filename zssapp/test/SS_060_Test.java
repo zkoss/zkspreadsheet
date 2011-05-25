006e17545abd34d52d78c6162b5c5e902fa65f3e
Sam Chuang <qk168899@gmail.com>
2011-05-25 05:55:37 +0000

    fixed test case

--- zssapp/test/SS_060_Test.java
+++ zssapp/test/SS_060_Test.java
@@ -8,8 +8,8 @@ public class SS_060_Test extends SSAbstractTestCase {
         JQuery cell_B_8 = getSpecifiedCell(1, 7);
         clickCell(cell_B_8);
         clickCell(cell_B_8);
-        click(jq("i.z-combobox-rounded-btn:eq(0)"));
-        JQuery fontItem = jq("td.z-comboitem-text:eq(3)");
+        click(jq("$fontCtrlPanel $fontFamily:visible i.z-combobox-rounded-btn"));
+        JQuery fontItem = jq(".z-combobox-rounded-pp .timeFont:visible");
         
         if (fontItem != null) {
             click(fontItem);
