adc2f666cab33d3841ab2af2786fbf765bcc1ba1
Sam Chuang <qk168899@gmail.com>
2011-05-25 08:49:31 +0000

    fixed test case

--- zssapp/test/SS_169_Test.java
+++ zssapp/test/SS_169_Test.java
@@ -4,12 +4,15 @@ public class SS_169_Test extends SSAbstractTestCase {
 
     @Override
     protected void executeTest() {
+    	verifyFalse(isWidgetVisible("$_openFileDialog"));
+    	
     	click("jq('$fileMenu button.z-menu-btn')");
     	waitResponse();
     	click("jq('$openFile a.z-menu-item-cnt')");
     	waitResponse();
     	
-    	// TODO verify if open file window is opened
-    	verifyTrue(widget(jq("$_openFileDialog div.z-window-highlighted-header")).exists());
+    	verifyTrue(isWidgetVisible("$_openFileDialog"));
+    	
+    	//TODO: need to open a excel file and verify
     }
 }
