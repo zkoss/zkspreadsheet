68fa6dcb283023a65e528710b602553bcbc02ec8
Sam Chuang <qk168899@gmail.com>
2011-05-25 04:32:20 +0000

    fixed test case

--- zssapp/test/SS_038_Test.java
+++ zssapp/test/SS_038_Test.java
@@ -4,12 +4,13 @@ public class SS_038_Test extends SSAbstractTestCase {
 
     @Override
     protected void executeTest() {
+    	verifyFalse(isWidgetVisible("$_insertFormulaDialog"));
+    	
     	click("jq('$insertMenu button.z-menu-btn')");
     	waitResponse();
     	click("jq('$insertFormula a.z-menu-item-cnt')");
     	waitResponse();
     	
-    	// TODO verify if insert formula window twindow is opened
-    	verifyTrue(widget(jq("@window[mode=\"highlighted\"][title=\"Insert Function\"] div.z-window-highlighted-header")).exists());
+    	verifyTrue(isWidgetVisible("$_insertFormulaDialog"));
     }
 }
