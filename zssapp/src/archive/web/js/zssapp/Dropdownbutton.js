f0712e5bbea3d7e4ad94246456ea9e8d5cb02533
Sam Chuang <qk168899@gmail.com>
2011-12-07 09:52:31 +0000

    zssapp: js code fine tune

--- zssapp/src/archive/web/js/zssapp/Dropdownbutton.js
+++ zssapp/src/archive/web/js/zssapp/Dropdownbutton.js
@@ -67,7 +67,7 @@ zssapp.Dropdownbutton = zk.$extends(zul.LabelImageWidget, {
 			zkn = zk(n),
 			w = zkn.offsetWidth(),
 			x = evt.domEvent.clientX - zkn.revisedOffset()[0];
-		return (x > (w - this._dpWidth));
+		return (x >= (w - this._dpWidth));
 	},
 	onChildAdded_: function (child) {
 		this.$supers('onChildAdded_', arguments);
