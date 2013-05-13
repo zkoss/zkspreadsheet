b09ff3ea77015df742ffd33e75186fd4428a6855
Hawk Chen <hawkchen@potix.com>
2013-05-13 18:06:27 +0800

    review operation test with 3.0 API

--- zss.test/src/test/java/zss/test/CellOperationTest.java
+++ zss.test/src/test/java/zss/test/CellOperationTest.java
@@ -6,13 +6,13 @@ import org.hamcrest.CoreMatchers;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ErrorCollector;
-import org.zkoss.poi.ss.usermodel.Cell;
-import org.zkoss.poi.ss.usermodel.CellStyle;
 import org.zkoss.zats.mimic.ComponentAgent;
 import org.zkoss.zats.mimic.DesktopAgent;
 import org.zkoss.zats.mimic.Zats;
-import org.zkoss.zss.model.Ranges;
-import org.zkoss.zss.model.Worksheet;
+import org.zkoss.zss.api.Range;
+import org.zkoss.zss.api.Ranges;
+import org.zkoss.zss.api.model.CellStyle;
+import org.zkoss.zss.api.model.Sheet;
 import org.zkoss.zss.ui.Spreadsheet;
 
 /**
@@ -24,7 +24,7 @@ public class CellOperationTest extends SpreadsheetTestCaseBase{
 	private DesktopAgent desktop;
 	private ComponentAgent zss;
 	private SpreadsheetAgent ssAgent;
-	Worksheet sheet;
+	private Sheet sheet;
 	
 	@Rule
     public ErrorCollector collector = new ErrorCollector();
@@ -41,17 +41,18 @@ public class CellOperationTest extends SpreadsheetTestCaseBase{
 	public void testCopy() {
 
 		//pre condition
-		String srcText = Ranges.range(sheet, 2, 1).getEditText();
-		String srcFormula = Ranges.range(sheet,3, 1).getEditText();
+		String srcText = Ranges.range(sheet, 2, 1).getCellEditText();
+		String srcFormula = Ranges.range(sheet,3, 1).getCellEditText();
 		
 		ssAgent.copy(2, 7, 1, 2);
 		ssAgent.paste(2, 4);
 		
 	
-		String dstText = Ranges.range(sheet, 2, 4).getEditText();
+		String dstText = Ranges.range(sheet, 2, 4).getCellEditText();
 		collector.checkThat(dstText, CoreMatchers.equalTo(srcText));
-		String dstFormula = Ranges.range(sheet,3, 4).getEditText();
+		String dstFormula = Ranges.range(sheet,3, 4).getCellEditText();
 		collector.checkThat(dstFormula, CoreMatchers.equalTo(srcFormula));
+		
 	}
 	
 	/**
@@ -60,12 +61,12 @@ public class CellOperationTest extends SpreadsheetTestCaseBase{
 	 */
 	@Test
 	public void testChangeBackgroundIssue() {
-		Cell mergedCell = getCell(sheet, 17, 1);
-		assertEquals(CellStyle.BORDER_NONE, mergedCell.getCellStyle().getBorderBottom());
+		Range mergedCell = Ranges.range(sheet, 17, 1);
+		assertEquals(CellStyle.BorderType.NONE, mergedCell.getCellStyle().getBorderBottom());
 		//TODO how to check it's merged
 		
 		ssAgent.fillBackgroundColor(18, 1, "ff0000");
 //		assertEquals(1, getCell(sheet,18,1).getCellStyle().getFillBackgroundColor());
-		assertEquals(CellStyle.BORDER_NONE, mergedCell.getCellStyle().getBorderBottom());
+		assertEquals(CellStyle.BorderType.NONE, mergedCell.getCellStyle().getBorderBottom());
 	}
 }
