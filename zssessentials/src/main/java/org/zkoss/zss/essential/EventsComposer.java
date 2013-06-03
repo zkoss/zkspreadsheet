04817e1fab9031fe6b7cc8ad51da467aec4e672e
Dennis <atticcat@gmail.com>
2013-06-03 12:58:06 +0800

    fine tune

--- zssessentials/src/main/java/org/zkoss/zss/essential/EventsComposer.java
+++ zssessentials/src/main/java/org/zkoss/zss/essential/EventsComposer.java
@@ -1,17 +1,27 @@
 package org.zkoss.zss.essential;
 
+import java.io.IOException;
+import java.util.ArrayList;
+
 import org.zkoss.zk.ui.Component;
+import org.zkoss.zk.ui.WebApps;
 import org.zkoss.zk.ui.select.SelectorComposer;
 import org.zkoss.zk.ui.select.annotation.Listen;
 import org.zkoss.zk.ui.select.annotation.Wire;
+import org.zkoss.zss.api.Importer;
+import org.zkoss.zss.api.Importers;
 import org.zkoss.zss.api.Ranges;
 import org.zkoss.zss.api.SheetAnchor;
+import org.zkoss.zss.api.model.Book;
 import org.zkoss.zss.api.model.Sheet;
+import org.zkoss.zss.ui.Spreadsheet;
+import org.zkoss.zss.ui.event.CellAreaEvent;
 import org.zkoss.zss.ui.event.CellEvent;
 import org.zkoss.zss.ui.event.CellMouseEvent;
 import org.zkoss.zss.ui.event.CellSelectionEvent;
 import org.zkoss.zss.ui.event.CellSelectionUpdateEvent;
 import org.zkoss.zss.ui.event.EditboxEditingEvent;
+import org.zkoss.zss.ui.event.Events;
 import org.zkoss.zss.ui.event.HeaderMouseEvent;
 import org.zkoss.zss.ui.event.HeaderUpdateEvent;
 import org.zkoss.zss.ui.event.KeyEvent;
@@ -22,7 +32,7 @@ import org.zkoss.zss.ui.event.StartEditingEvent;
 import org.zkoss.zss.ui.event.StopEditingEvent;
 import org.zkoss.zss.ui.event.WidgetKeyEvent;
 import org.zkoss.zss.ui.event.WidgetUpdateEvent;
-import org.zkoss.zul.ListModel;
+import org.zkoss.zul.Grid;
 import org.zkoss.zul.ListModelList;
 import org.zkoss.zul.Listbox;
 
@@ -37,47 +47,170 @@ public class EventsComposer extends SelectorComposer<Component> {
 
 	ListModelList<String> infoModel = new ListModelList<String>();
 	
+	ListModelList<String> bookModel = new ListModelList<String>();
+	
+	ListModelList<String> eventFilterModel = new ListModelList<String>();
+	
 	@Wire
-	Listbox infolist;
+	Grid infolist;
+	
+	@Wire
+	Listbox booklist;
+	
+	@Wire
+	Listbox eventlist;
+	
+	@Wire
+	Spreadsheet ss;
+	
+	
 	
 	
 	@Override
 	public void doAfterCompose(Component comp) throws Exception {
 		super.doAfterCompose(comp);
 		
+		initModel();
+		booklist.setModel(bookModel);
 		infolist.setModel(infoModel);
+		eventlist.setModel(eventFilterModel);
 		
-		addEventInfo("Spreadsheet initialized");
+		addInfo("Spreadsheet initialized");
 	}
 
-	private void addEventInfo(String info){
+	private void initModel() {
+		bookModel.add("blank.xlsx");
+		bookModel.add("sample.xlsx");
+		
+		
+		//Available events
+		//It is just for showing message, event is always listened in this demo.
+		eventFilterModel.setMultiple(true);
+		
+		addEventFilter(Events.ON_AUX_ACTION,true);
+		
+		addEventFilter(Events.ON_CELL_CHANGE,true);
+		addEventFilter(Events.ON_CELL_FOUCSED,false);
+		addEventFilter(Events.ON_CELL_SELECTION,false);
+		addEventFilter(Events.ON_CELL_SELECTION_UPDATE,true);
+		
+		
+		
+		addEventFilter(Events.ON_CELL_CLICK,false);
+		addEventFilter(Events.ON_CELL_DOUBLE_CLICK,true);
+		addEventFilter(Events.ON_CELL_RIGHT_CLICK,true);
+		
+//		addEventFilter(Events.ON_CELL_FILTER);//useless
+//		addEventFilter(Events.ON_CELL_VALIDATOR);//useless
+		
+		addEventFilter(Events.ON_START_EDITING,true);
+		addEventFilter(Events.ON_EDITBOX_EDITING,true);
+		addEventFilter(Events.ON_STOP_EDITING,true);
+		
+		
+		addEventFilter(Events.ON_HEADER_UPDATE,true);
+		addEventFilter(Events.ON_HEADER_CLICK,true);
+		addEventFilter(Events.ON_HEADER_RIGHT_CLICK,true);
+		addEventFilter(Events.ON_HEADER_DOUBLE_CLICK,true);
+		
+		addEventFilter(Events.ON_CTRL_KEY,true);
+		
+		addEventFilter(Events.ON_WIDGET_UPDATE,true);
+		addEventFilter(Events.ON_WIDGET_CTRL_KEY,true);
+		
+		addEventFilter(Events.ON_SHEET_CREATE,true);
+		addEventFilter(Events.ON_SHEET_DELETE,true);
+		addEventFilter(Events.ON_SHEET_NAME_CHANGE,true);
+		addEventFilter(Events.ON_SHEET_ORDER_CHANGE,true);
+		addEventFilter(Events.ON_SHEET_SELECT,true);
+		
+		addEventFilter(Events.ON_HYPERLINK,true);	
+		
+		//add default show only
+		
+	}
+	
+	private void addEventFilter(String event,boolean showinfo){
+		if(!eventFilterModel.contains(eventFilterModel)){
+			eventFilterModel.add(event);
+		}
+		if(showinfo){
+			eventFilterModel.addToSelection(event);
+		}else{
+			eventFilterModel.removeFromSelection(event);
+		}
+	}
+	
+	private boolean isShowEventInfo(String event){
+		return eventFilterModel.getSelection().contains(event);
+	}
+
+	private void addInfo(String info){
 		infoModel.add(0, info);
 		while(infoModel.size()>100){
 			infoModel.remove(infoModel.size()-1);
 		}
 	}
 	
+	@Listen("onSelect = #booklist")
+	public void onBookSelect(){
+		String bookname = bookModel.getSelection().iterator().next();
+		
+		Importer imp = Importers.getImporter("excel");
+		try {
+			Book book = imp.imports(WebApps.getCurrent().getResource("/WEB-INF/books/"+bookname), bookname);
+			ss.setBook(book);
+		} catch (IOException e) {
+			throw new RuntimeException(e.getMessage(),e);
+		}
+	}
+	@Listen("onClick = #clearInfo")
+	public void onClearInfo(){
+		infoModel.clear();
+	}
+	@Listen("onClick = #clearAllFilter")
+	public void onClearAllFilter(){
+		eventFilterModel.clearSelection();
+	}
+	@Listen("onClick = #selectAllFilter")
+	public void onSelectorAll(){
+		eventFilterModel.clearSelection();
+		eventFilterModel.setSelection(new ArrayList<String>(eventFilterModel));
+	}
+	
+	
+	//Events
+	
 	@Listen("onCellFocused = #ss")
 	public void onCellFocused(CellEvent event){
 		StringBuilder info = new StringBuilder();
-		info.append("Focus on[").append(Ranges.toCellReference(event.getRow(),event.getColumn())).append("]");
-		addEventInfo(info.toString());
+		info.append("Focus on[").append(Ranges.getCellReference(event.getRow(),event.getColumn())).append("]");
+		
+		if(isShowEventInfo(event.getName())){
+			addInfo(info.toString());
+		}
 	}
 	
 	@Listen("onCellSelection = #ss")
 	public void onCellSelection(CellSelectionEvent event){
 		StringBuilder info = new StringBuilder();
-		info.append("Select on[").append(Ranges.toAreaReference(event.getArea())).append("]");
-		addEventInfo(info.toString());
+		info.append("Select on[").append(Ranges.getAreaReference(event.getArea())).append("]");
+		
+		if(isShowEventInfo(event.getName())){
+			addInfo(info.toString());
+		}
 	}
 	
 	@Listen("onCellSelectionUpdate = #ss")
 	public void onCellSelectionUpdate(CellSelectionUpdateEvent event){
 		StringBuilder info = new StringBuilder();
 		info.append("Selection update from[")
-				.append(Ranges.toAreaReference(event.getOrigRow(),event.getOrigColumn(), event.getOrigLastRow(),event.getOrigLastColumn())).append("] to [")
-				.append(Ranges.toAreaReference(event.getArea())).append("]");
-		addEventInfo(info.toString());
+				.append(Ranges.getAreaReference(event.getOrigRow(),event.getOrigColumn(), event.getOrigLastRow(),event.getOrigLastColumn())).append("] to [")
+				.append(Ranges.getAreaReference(event.getArea())).append("]");
+		
+		if(isShowEventInfo(event.getName())){
+			addInfo(info.toString());
+		}
 	}
 
 	@Listen("onHeaderUpdate = #ss")
@@ -87,7 +220,7 @@ public class EventsComposer extends SelectorComposer<Component> {
 			.append(" on ").append(event.getType());
 		switch(event.getType()){
 		case COLUMN:
-			info.append(" ").append(Ranges.toColumnReference(event.getIndex()));
+			info.append(" ").append(Ranges.getColumnReference(event.getIndex()));
 			break;
 		case ROW:
 			info.append(" ").append(Ranges.toRowReference(event.getIndex()));
@@ -103,7 +236,10 @@ public class EventsComposer extends SelectorComposer<Component> {
 			}
 			break;
 		}
-		addEventInfo(info.toString());
+		
+		if(isShowEventInfo(event.getName())){
+			addInfo(info.toString());
+		}
 	}
 	
 	
@@ -114,13 +250,16 @@ public class EventsComposer extends SelectorComposer<Component> {
 		
 		switch(event.getType()){
 		case COLUMN:
-			info.append(Ranges.toColumnReference(event.getIndex()));
+			info.append(Ranges.getColumnReference(event.getIndex()));
 			break;
 		case ROW:
 			info.append(Ranges.toRowReference(event.getIndex()));
 			break;
 		}
-		addEventInfo(info.toString());
+		
+		if(isShowEventInfo(event.getName())){
+			addInfo(info.toString());
+		}
 	}
 	@Listen("onHeaderRightClick = #ss")
 	public void onHeaderRightClick(HeaderMouseEvent event){
@@ -129,13 +268,16 @@ public class EventsComposer extends SelectorComposer<Component> {
 		
 		switch(event.getType()){
 		case COLUMN:
-			info.append(Ranges.toColumnReference(event.getIndex()));
+			info.append(Ranges.getColumnReference(event.getIndex()));
 			break;
 		case ROW:
 			info.append(Ranges.toRowReference(event.getIndex()));
 			break;
 		}
-		addEventInfo(info.toString());
+		
+		if(isShowEventInfo(event.getName())){
+			addInfo(info.toString());
+		}
 	}
 	@Listen("onHeaderDoubleClick = #ss")
 	public void onHeaderDoubleClick(HeaderMouseEvent event){
@@ -144,102 +286,127 @@ public class EventsComposer extends SelectorComposer<Component> {
 		
 		switch(event.getType()){
 		case COLUMN:
-			info.append(Ranges.toColumnReference(event.getIndex()));
+			info.append(Ranges.getColumnReference(event.getIndex()));
 			break;
 		case ROW:
 			info.append(Ranges.toRowReference(event.getIndex()));
 			break;
 		}
-		addEventInfo(info.toString());
+		
+		if(isShowEventInfo(event.getName())){
+			addInfo(info.toString());
+		}
 	}
 	
 	@Listen("onCellClick = #ss")
 	public void onCellClick(CellMouseEvent event){
 		StringBuilder info = new StringBuilder();
-		info.append("Click on cell ").append(Ranges.toCellReference(event.getRow(),event.getColumn()));
+		info.append("Click on cell ").append(Ranges.getCellReference(event.getRow(),event.getColumn()));
 		
-		addEventInfo(info.toString());
+		if(isShowEventInfo(event.getName())){
+			addInfo(info.toString());
+		}
 	}
 	@Listen("onCellRightClick = #ss")
 	public void onCellRightClick(CellMouseEvent event){
 		StringBuilder info = new StringBuilder();
-		info.append("Right-click on cell ").append(Ranges.toCellReference(event.getRow(),event.getColumn()));
+		info.append("Right-click on cell ").append(Ranges.getCellReference(event.getRow(),event.getColumn()));
 		
-		addEventInfo(info.toString());
+		if(isShowEventInfo(event.getName())){
+			addInfo(info.toString());
+		}
 	}
 	@Listen("onCellDoubleClick = #ss")
 	public void onCellDoubleClick(CellMouseEvent event){
 		StringBuilder info = new StringBuilder();
-		info.append("Double-click on cell ").append(Ranges.toCellReference(event.getRow(),event.getColumn()));
+		info.append("Double-click on cell ").append(Ranges.getCellReference(event.getRow(),event.getColumn()));
 		
-		addEventInfo(info.toString());
+		if(isShowEventInfo(event.getName())){
+			addInfo(info.toString());
+		}
 	}
 	
 	@Listen("onStartEditing = #ss")
 	public void onStartEditing(StartEditingEvent event){
 		StringBuilder info = new StringBuilder();
-		String ref = Ranges.toCellReference(event.getRow(),event.getColumn());
+		String ref = Ranges.getCellReference(event.getRow(),event.getColumn());
 		info.append("Start to edit ").append(ref)
 		.append(", editing-value is ").append(event.getEditingValue()).append(" client-value is ").append(event.getClientValue());
-		addEventInfo(info.toString());
+		
+		if(isShowEventInfo(event.getName())){
+			addInfo(info.toString());
+		}
 		
 		if(ref.equals("D1")){
 			String newval = "Supprise!!";
 			//we change the editing value
 			event.setEditingValue(newval);
-			addEventInfo("Editing value is change to "+newval);
+			addInfo("Editing value is change to "+newval);
 		}else if(ref.equals("E1")){
 			//we don't allow edit D2
 			event.cancel();
-			addEventInfo("Editing is canceled");
+			addInfo("Editing is canceled");
 		}else{
-			addEventInfo("Try to edit D1 or E1");
+			addInfo("Try to edit D1 or E1");
 		}
 	}
 	
 	@Listen("onStopEditing = #ss")
 	public void onStopEditing(StopEditingEvent event){
 		StringBuilder info = new StringBuilder();
-		String ref = Ranges.toCellReference(event.getRow(),event.getColumn());
+		String ref = Ranges.getCellReference(event.getRow(),event.getColumn());
 		info.append("Stop editing ").append(ref)
 		.append(", value is ").append(event.getEditingValue());
-		addEventInfo(info.toString());
+		
+		if(isShowEventInfo(event.getName())){
+			addInfo(info.toString());
+		}
 		
 		if(ref.equals("D3")){
 			String newval = event.getEditingValue()+"-Woo";
 			//we change the editing value
 			event.setEditingValue(newval);
-			addEventInfo("Editing value is changing to "+newval);
+			addInfo("Editing value is changing to "+newval);
 		}else if(ref.equals("E3")){
 			//we don't allow edit D2
 			event.cancel();
-			addEventInfo("Editing is canceled");
+			addInfo("Editing is canceled");
 		}else{
-			addEventInfo("Try to edit D3 or E3");
+			addInfo("Try to edit D3 or E3");
 		}
 	}
 	
 	@Listen("onEditboxEditing = #ss")
 	public void onEditboxEditing(EditboxEditingEvent event){
 		StringBuilder info = new StringBuilder();
-		String ref = Ranges.toCellReference(event.getRow(),event.getColumn());
+		String ref = Ranges.getCellReference(event.getRow(),event.getColumn());
 		info.append("Editing ").append(ref)
 		.append(", value is ").append(event.getEditingValue());
-		addEventInfo(info.toString());
+		
+		if(isShowEventInfo(event.getName())){
+			addInfo(info.toString());
+		}
 	}
 	
 	@Listen("onSheetSelect = #ss")
 	public void onSheetSelect(SheetSelectEvent event){
 		StringBuilder info = new StringBuilder();
 		info.append("Select sheet : ").append(event.getSheetName());
-		addEventInfo(info.toString());
+		
+		if(isShowEventInfo(event.getName())){
+			addInfo(info.toString());
+		}
 	}
 	
 	@Listen("onSheetNameChange = #ss")
 	public void onSheetNameChange(SheetEvent event){
 		StringBuilder info = new StringBuilder();
 		info.append("Rename sheet to ").append(event.getSheetName());
-		addEventInfo(info.toString());
+		
+		
+		if(isShowEventInfo(event.getName())){
+			addInfo(info.toString());
+		}
 	}
 	
 	@Listen("onSheetOrderChange = #ss")
@@ -247,21 +414,42 @@ public class EventsComposer extends SelectorComposer<Component> {
 		StringBuilder info = new StringBuilder();
 		Sheet sheet = event.getSheet();
 		info.append("Reorder sheet : ").append(event.getSheetName()).append(" to ").append(sheet.getBook().getSheetIndex(sheet));
-		addEventInfo(info.toString());
+		
+		if(isShowEventInfo(event.getName())){
+			addInfo(info.toString());
+		}
 	}
 	
 	@Listen("onSheetCreate = #ss")
 	public void onSheetCreate(SheetEvent event){
 		StringBuilder info = new StringBuilder();
 		info.append("Create sheet : ").append(event.getSheetName());
-		addEventInfo(info.toString());
+		
+		if(isShowEventInfo(event.getName())){
+			addInfo(info.toString());
+		}
 	}
 	
 	@Listen("onSheetDelete = #ss")
 	public void onSheetDelete(SheetDeleteEvent event){
 		StringBuilder info = new StringBuilder();
 		info.append("Delete sheet : ").append(event.getSheetName());
-		addEventInfo(info.toString());
+		
+		if(isShowEventInfo(event.getName())){
+			addInfo(info.toString());
+		}
+	}
+	
+	@Listen("onCellChange = #ss")
+	public void onCellChange(CellAreaEvent event){
+		StringBuilder info = new StringBuilder();
+		
+		info.append("Cell changes on ").append(Ranges.getAreaReference(event.getArea()));
+		info.append(", first value is :"+Ranges.range(event.getSheet(),event.getArea()).getCellFormatText());
+		
+		if(isShowEventInfo(event.getName())){
+			addInfo(info.toString());
+		}
 	}
 	
 	@Listen("onCtrlKey = #ss")
@@ -273,7 +461,9 @@ public class EventsComposer extends SelectorComposer<Component> {
 			.append(", Alt:").append(event.isAltKey())
 			.append(", Shift:").append(event.isShiftKey());
 		
-		addEventInfo(info.toString());
+		if(isShowEventInfo(event.getName())){
+			addInfo(info.toString());
+		}
 	}
 	
 	@Listen("onWidgetUpdate = #ss")
@@ -286,11 +476,13 @@ public class EventsComposer extends SelectorComposer<Component> {
 				.append(" ")
 				.append(event.getAction())
 				.append(" to ")
-				.append(Ranges.toAreaReference(anchor.getRow(),
+				.append(Ranges.getAreaReference(anchor.getRow(),
 						anchor.getColumn(), anchor.getLastRow(),
 						anchor.getLastColumn()));
 		
-		addEventInfo(info.toString());
+		if(isShowEventInfo(event.getName())){
+			addInfo(info.toString());
+		}
 	}
 	
 	@Listen("onWidgetCtrlKey = #ss")
@@ -303,29 +495,17 @@ public class EventsComposer extends SelectorComposer<Component> {
 			.append(", Alt:").append(event.isAltKey())
 			.append(", Shift:").append(event.isShiftKey());
 		
-		addEventInfo(info.toString());
+		if(isShowEventInfo(event.getName())){
+			addInfo(info.toString());
+		}
 	}
 	
 	
 	/**
-	 * ON_CTRL_KEY
 	 * ON_AUX_ACTION
 	 * ON_HYPERLINK
 	 * ON_CELL_FILTER //useless
 	 * ON_CELL_VALIDATOR //useless
-	 * 
-	 * ==Widget Events==
-	 * ON_WIDGET_CTRL_KEY
-	 * ON_WIDGET_UPDATE
-	 * 
-	 * ==BookEvents==
-	 * ON_SHEET_NAME_CHANGE
-	 * ON_SHEET_ORDER_CHANGE
-	 * ON_SHEET_DELETE
-	 * ON_SHEET_CREATE
-	 * ON_CELL_CHANGE
-	 * 
-	 * 
 	 */
 }
 
