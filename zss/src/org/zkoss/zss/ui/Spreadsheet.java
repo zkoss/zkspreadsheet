60f0cf567b276a11bdee8c5077d3a51cdcd8690a
pao <paowang@potix.com>
2013-06-21 14:26:27 +0800

    ZSS-351:  * move the force flag logic from validation helper to outside.  * don't share validation helper instance

--- zss/src/org/zkoss/zss/ui/Spreadsheet.java
+++ zss/src/org/zkoss/zss/ui/Spreadsheet.java
@@ -306,8 +306,6 @@ public class Spreadsheet extends XulElement implements Serializable, AfterCompos
 //	
 //	private static Set<UserAction> _defToolbarActiobDisabled;
 	
-	private ValidationHelper _validationHelper = new ValidationHelper(this); // ZSS-351: validation helper has status to support force changed case 
-	
 	public Spreadsheet() {
 		this.addEventListener("onStartEditingImpl", new EventListener() {
 			public void onEvent(Event event) throws Exception {
@@ -4360,11 +4358,16 @@ public class Spreadsheet extends XulElement implements Serializable, AfterCompos
 		});
 	}
 
+	// a local flag indicates that skip the validation and force this editing (ZSS-351) 
+	private boolean forceStopEditing0 = false; 
+	
 	private void processStopEditing0(final String token, final Sheet sheet, final int rowIdx, final int colIdx, final Object value, final String editingType) {
 		try {
 			
+			ValidationHelper validationHelper = new ValidationHelper(this);
 			String editText = value == null ? "" : value.toString();
-			if (!_validationHelper.validate(sheet, rowIdx, colIdx, editText,
+			// ZSS-351: use force flag to skip the validation when user want force this editing
+			if (!forceStopEditing0 && !validationHelper.validate(sheet, rowIdx, colIdx, editText,
 				//callback
 				new EventListener() {
 					@Override
@@ -4373,7 +4376,12 @@ public class Spreadsheet extends XulElement implements Serializable, AfterCompos
 						if (Messagebox.ON_CANCEL.equals(eventname)) { //cancel
 							Spreadsheet.this.processCancelEditing0(token, sheet, rowIdx, colIdx, true, editingType); //skipMove
 						} else if (Messagebox.ON_OK.equals(eventname)) { //ok
-							Spreadsheet.this.processStopEditing0(token, sheet, rowIdx, colIdx, value, editingType);
+							try {
+								forceStopEditing0 = true;
+								Spreadsheet.this.processStopEditing0(token, sheet, rowIdx, colIdx, value, editingType);
+							} finally {
+								forceStopEditing0 = false;
+							}
 						} else { //retry
 							Spreadsheet.this.processRetryEditing0(token, sheet, rowIdx, colIdx, value, editingType);
 						}
@@ -4806,7 +4814,7 @@ public class Spreadsheet extends XulElement implements Serializable, AfterCompos
 	@Deprecated
 	public boolean validate(XSheet sheet, final int row, final int col, final String txt, 
 		final EventListener callback) {
-		return _validationHelper.validate(new SheetImpl(new SimpleRef<XBook>(sheet.getBook()),new SimpleRef<XSheet>(sheet)), row, col, txt, callback);
+		return new ValidationHelper(this).validate(new SheetImpl(new SimpleRef<XBook>(sheet.getBook()),new SimpleRef<XSheet>(sheet)), row, col, txt, callback);
 	}
 ////		final XSheet ssheet = this.getSelectedXSheet();
 ////		if (ssheet == null || !ssheet.equals(sheet)) { //skip no sheet case
