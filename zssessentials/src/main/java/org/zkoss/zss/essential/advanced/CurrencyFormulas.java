54efcc7be1c0fa77404de60f8953e67b34ab1703
Hawk Chen <hawkchen@potix.com>
2013-07-17 14:05:03 +0800

    add custom formula example

--- zssessentials/src/main/java/org/zkoss/zss/essential/advanced/CurrencyFormulas.java
+++ zssessentials/src/main/java/org/zkoss/zss/essential/advanced/CurrencyFormulas.java
@@ -1,5 +1,8 @@
 package org.zkoss.zss.essential.advanced;
 
+import org.zkoss.poi.ss.formula.eval.ValueEval;
+import org.zkoss.poi.ss.formula.functions.AggregateFunction;
+
 /**
  * Convert USD to TWD upon exchange rate.
  * @author Hawk
@@ -9,4 +12,5 @@ public class CurrencyFormulas {
 	public static double usd2Twd(double usd, double exchangeRate) {
         return usd * exchangeRate;
     }
+	
 }
