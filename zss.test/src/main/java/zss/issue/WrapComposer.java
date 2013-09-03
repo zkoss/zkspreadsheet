08b6b63c075587b97cbef1f54100174a8410e746
Hawk Chen <hawkchen@potix.com>
2013-09-03 16:23:30 +0800

    modify ZSS-241 test app for 3.0.0.RC

--- zss.test/src/main/java/zss/issue/WrapComposer.java
+++ zss.test/src/main/java/zss/issue/WrapComposer.java
@@ -4,7 +4,8 @@ import org.zkoss.zk.ui.Component;
 import org.zkoss.zk.ui.select.SelectorComposer;
 import org.zkoss.zk.ui.select.annotation.Listen;
 import org.zkoss.zk.ui.select.annotation.Wire;
-import org.zkoss.zss.ui.event.HeaderEvent;
+import org.zkoss.zss.ui.event.HeaderUpdateEvent;
+import org.zkoss.zss.ui.event.HedaerType;
 import org.zkoss.zul.Label;
 import org.zkoss.zul.Vlayout;
 
@@ -14,9 +15,9 @@ public class WrapComposer extends SelectorComposer<Component>{
 	@Wire
 	private Vlayout msgArea;
 
-	@Listen("onHeaderSize = spreadsheet")
-	public void printHeaderResizeInfo(HeaderEvent event){
-		if (event.getType() == HeaderEvent.LEFT_HEADER){
+	@Listen("onHeaderUpdate = spreadsheet")
+	public void printHeaderResizeInfo(HeaderUpdateEvent event){
+		if (event.getType() == HedaerType.ROW){
 			Label messageLabel = new Label("resize row" + (event.getIndex()+1)+" to "+event.getData());
 			msgArea.insertBefore(messageLabel, msgArea.getFirstChild());
 		}
