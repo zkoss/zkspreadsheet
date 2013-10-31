1d3dd5be2a854221de061a02051d0ba7bd109310
Dennis <atticcat@gmail.com>
2013-10-31 18:49:23 +0800

    ZSS-491, refactor sparedsheet ghost/widget client LOD to correct ZK way

--- zss/src/archive/web/js/zss/mold/spreadsheet.js
+++ zss/src/archive/web/js/zss/mold/spreadsheet.js
@@ -4,6 +4,11 @@ function (out) {
 	out.push('<div ', this.domAttrs_(), '>', tbp ? tbp.redrawHTML_() : '');
 	out.push(this.cave.redraw(out));//sheet
 	
+	for (var w = this.firstChild; w; w = w.nextSibling){
+		if(w.ghost){//allow to draw ghost to main body
+			w.redraw(out);
+		}
+	}
 	
     out.push('</div>');
 }
