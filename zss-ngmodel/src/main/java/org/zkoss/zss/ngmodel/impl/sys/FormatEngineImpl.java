8570000877f92f4a304f11f0f57dcc4fad1b1008
Hawk Chen <hawkchen@potix.com>
2013-12-05 09:37:42 +0800

    synchronize  locale from FormatContext to ZssContext.

--- zss-ngmodel/src/main/java/org/zkoss/zss/ngmodel/impl/sys/FormatEngineImpl.java
+++ zss-ngmodel/src/main/java/org/zkoss/zss/ngmodel/impl/sys/FormatEngineImpl.java
@@ -1,6 +1,7 @@
 package org.zkoss.zss.ngmodel.impl.sys;
 
 import org.zkoss.poi.ss.format.CellFormat;
+import org.zkoss.poi.ss.usermodel.ZssContext;
 import org.zkoss.zss.ngmodel.sys.format.FormatContext;
 import org.zkoss.zss.ngmodel.sys.format.FormatEngine;
 import org.zkoss.zss.ngmodel.sys.format.FormatResult;
@@ -9,8 +10,16 @@ public class FormatEngineImpl implements FormatEngine {
 
 	@Override
 	public FormatResult format(String format, Object value, FormatContext context){
-		CellFormat formatter = CellFormat.getInstance(format, context.getLocale());
-		return new FormatResultImpl(formatter.apply(value));
+		ZssContext old = ZssContext.getThreadLocal();
+		try{
+			ZssContext zssContext = old==null?new ZssContext(context.getLocale(),-1): new ZssContext(context.getLocale(),old.getTwoDigitYearUpperBound());
+			ZssContext.setThreadLocal(zssContext);
+			
+			CellFormat formatter = CellFormat.getInstance(format, context.getLocale());
+			return new FormatResultImpl(formatter.apply(value));
+		}finally{
+			ZssContext.setThreadLocal(old);
+		}
 	}
 
 }
--- zss-ngmodel/src/main/java/org/zkoss/zss/ngmodel/sys/format/FormatEngine.java
+++ zss-ngmodel/src/main/java/org/zkoss/zss/ngmodel/sys/format/FormatEngine.java
@@ -1,6 +1,5 @@
 package org.zkoss.zss.ngmodel.sys.format;
 
-import org.zkoss.zss.ngmodel.NCell;
 
 public interface FormatEngine {
 
