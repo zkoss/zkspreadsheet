dfc04024e095da18a459e03e08b529d69c045724
Dennis Chen <atticcat@gmail.com>
2014-01-21 18:46:16 +0800

    fix merge issue

--- zss-ngmodel/src/test/java/org/zkoss/zss/ngmodel/ModelFormulaTest.java
+++ zss-ngmodel/src/test/java/org/zkoss/zss/ngmodel/ModelFormulaTest.java
@@ -73,9 +73,94 @@ public class ModelFormulaTest {
 		Assert.assertEquals(9D, sheet1.getCell("B2").getValue());
 	}
 	
-//	@Test
-	public void testFormulaDependencyClearAfterInsertDeleteCell(){
-		//TODO
+	@Test
+	public void testFormulaDependencyClearAfterInsertDeleteCellVertical(){
+		NBook book = NBooks.createBook("book1");
+		book.getBookSeries().setAutoFormulaCacheClean(true);
+		DependencyTable dt = ((AbstractBookSeriesAdv)book.getBookSeries()).getDependencyTable();
+		NSheet sheet1 = initialDataGrid(book.createSheet("Sheet1"));
+		
+		sheet1.getCell("A1").setValue(3);
+		sheet1.getCell("C3").setValue("=A1");
+		
+		Assert.assertEquals(3D, sheet1.getCell("C3").getValue());
+		
+		sheet1.getCell("A1").setValue(5);
+		Assert.assertEquals(5D, sheet1.getCell("C3").getValue());
+		
+		
+		Set<Ref> refs = dt.getDependents(new RefImpl((AbstractCellAdv)sheet1.getCell("A1")));
+		Assert.assertEquals(1, refs.size());
+		Ref ref = refs.iterator().next();
+		CellRegion refRegion = new CellRegion(ref.getRow(),ref.getColumn(),ref.getLastRow(),ref.getLastColumn());
+		Assert.assertEquals("C3",refRegion.getReferenceString());
+		
+		sheet1.insertCell(new CellRegion("C3"),false);
+		refs = dt.getDependents(new RefImpl((AbstractCellAdv)sheet1.getCell("A1")));
+		Assert.assertEquals(1, refs.size());
+		ref = refs.iterator().next();
+		refRegion = new CellRegion(ref.getRow(),ref.getColumn(),ref.getLastRow(),ref.getLastColumn());
+		Assert.assertEquals("C4",refRegion.getReferenceString());
+		
+		sheet1.getCell("A1").setValue(7);
+		Assert.assertEquals(null, sheet1.getCell("C3").getValue());
+		Assert.assertEquals(7D, sheet1.getCell("C4").getValue());
+		
+		sheet1.deleteCell(new CellRegion("C2:C3"),false);
+		refs = dt.getDependents(new RefImpl((AbstractCellAdv)sheet1.getCell("A1")));
+		Assert.assertEquals(1, refs.size());
+		ref = refs.iterator().next();
+		refRegion = new CellRegion(ref.getRow(),ref.getColumn(),ref.getLastRow(),ref.getLastColumn());
+		Assert.assertEquals("C2",refRegion.getReferenceString());
+		
+		sheet1.getCell("A1").setValue(9);
+		Assert.assertEquals(null, sheet1.getCell("C4").getValue());
+		Assert.assertEquals(9D, sheet1.getCell("C2").getValue());
+	}
+	
+	@Test
+	public void testFormulaDependencyClearAfterInsertDeleteCellHorzontal(){
+		NBook book = NBooks.createBook("book1");
+		book.getBookSeries().setAutoFormulaCacheClean(true);
+		DependencyTable dt = ((AbstractBookSeriesAdv)book.getBookSeries()).getDependencyTable();
+		NSheet sheet1 = initialDataGrid(book.createSheet("Sheet1"));
+		
+		sheet1.getCell("A1").setValue(3);
+		sheet1.getCell("C3").setValue("=A1");
+		
+		Assert.assertEquals(3D, sheet1.getCell("C3").getValue());
+		
+		sheet1.getCell("A1").setValue(5);
+		Assert.assertEquals(5D, sheet1.getCell("C3").getValue());
+		
+		
+		Set<Ref> refs = dt.getDependents(new RefImpl((AbstractCellAdv)sheet1.getCell("A1")));
+		Assert.assertEquals(1, refs.size());
+		Ref ref = refs.iterator().next();
+		CellRegion refRegion = new CellRegion(ref.getRow(),ref.getColumn(),ref.getLastRow(),ref.getLastColumn());
+		Assert.assertEquals("C3",refRegion.getReferenceString());
+		
+		sheet1.insertCell(new CellRegion("C3"),true);
+		refs = dt.getDependents(new RefImpl((AbstractCellAdv)sheet1.getCell("A1")));
+		Assert.assertEquals(1, refs.size());
+		ref = refs.iterator().next();
+		refRegion = new CellRegion(ref.getRow(),ref.getColumn(),ref.getLastRow(),ref.getLastColumn());
+		Assert.assertEquals("D3",refRegion.getReferenceString());
+		
+		sheet1.getCell("A1").setValue(7);
+		Assert.assertEquals(null, sheet1.getCell("C3").getValue());
+		Assert.assertEquals(7D, sheet1.getCell("D3").getValue());
+		
+		sheet1.deleteCell(new CellRegion("B3:C3"),true);
+		refs = dt.getDependents(new RefImpl((AbstractCellAdv)sheet1.getCell("A1")));
+		Assert.assertEquals(1, refs.size());
+		ref = refs.iterator().next();
+		refRegion = new CellRegion(ref.getRow(),ref.getColumn(),ref.getLastRow(),ref.getLastColumn());
+		Assert.assertEquals("B3",refRegion.getReferenceString());
+		
+		sheet1.getCell("A1").setValue(9);
+		Assert.assertEquals(null, sheet1.getCell("D3").getValue());
+		Assert.assertEquals(9D, sheet1.getCell("B3").getValue());
 	}
 	
 	@Test
