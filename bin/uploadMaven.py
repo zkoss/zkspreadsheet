5adcf96e903790a27b92b47bb6d211c15a44042a
Hawk Chen <hawkchen@potix.com>
2014-02-12 09:20:16 +0800

    modify upload maven script to upload proprietary maven bundles

--- bin/uploadMaven.py
+++ bin/uploadMaven.py
@@ -13,16 +13,19 @@ import shutil
 ZSS_VERSION = None 
 ZPOI_VERSION = None
 
+isFreshlyVersion = True
 
-ZSS_BUNDLE_FILE_PATH = '/zss/maven/FL/' #FIXME not for proprietary
-ZPOI_BUNDLE_FILE_PATH = '/zpoi/maven/FL/'
+
+ZSS_MAVEN_PATH = '/zss/maven/' 
+ZPOI_MAVEN_PATH = '/zpoi/maven/'
+
 
 LOCAL_RELEASE_PATH = "/tmp"
 REMOTE_RELEASE_PATH = "//10.1.3.252/potix/rd"
 MOUNTED_RELEASE_PATH = LOCAL_RELEASE_PATH+"/potix-rd/"
 
-zssList = ['zss', 'zssex', 'zsshtml', 'zssjsf','zssjsp','zsspdf']
-zpoiList = ['zpoi', 'zpoiex']
+ZSS_PROJECT_LIST = ['zss', 'zssex', 'zsshtml', 'zssjsf','zssjsp','zsspdf']
+ZPOI_PROJECT_LIST = ['zpoi', 'zpoiex']
 
 
 
@@ -38,31 +41,53 @@ def mountRemoteFolder():
 
 # initialize zss, zpoi version from released file names
 def initProjectVersion():
-	freshlyVersionRegExpr = re.compile("(\d\.)+FL\.(\d)+")
-	#officialVersionRe =
-	
-	print("Get freshly version")
-	#find zpoi version
+	#officialVersionRe = 
+	global isFreshlyVersion
+	if os.path.exists(LOCAL_RELEASE_PATH+ZSS_MAVEN_PATH+'proprietary'):
+		isFreshlyVersion = False
+		print("Official version")
+	else:
+		isFreshlyVersion = True
+		print("Freshly version")
+	zssBundleFileName = os.listdir(getBundleFilePath(ZSS_PROJECT_LIST[0]))[0]
+	zpoiBundleFileName = os.listdir(getBundleFilePath(ZPOI_PROJECT_LIST[0]))[0]
 	global ZPOI_VERSION 
-	ZPOI_VERSION = freshlyVersionRegExpr.search(os.listdir(getBundleFilePath(zpoiList[0]))[0]).group(0)
-	print("zpoi version: "+ZPOI_VERSION)
-	#find zss version
 	global ZSS_VERSION 
-	ZSS_VERSION = freshlyVersionRegExpr.search(os.listdir(getBundleFilePath(zssList[0]))[0]).group(0)
-	print("zss version: "+ZSS_VERSION)
-
+	if (isFreshlyVersion):
+		freshlyVersionRegExpr = re.compile("(\d\.)+FL\.(\d)+")
+		ZPOI_VERSION = freshlyVersionRegExpr.search(zpoiBundleFileName).group(0)
+		print("zpoi version: "+ZPOI_VERSION)
+		ZSS_VERSION = freshlyVersionRegExpr.search(zssBundleFileName).group(0)
+		print("zss version: "+ZSS_VERSION)
+	else:
+		officialVersionRegExpr = re.compile("\d\.(\d)+\.(\d)+")
+		ZPOI_VERSION = officialVersionRegExpr.search(zpoiBundleFileName).group(0)
+		print("zpoi version: "+ZPOI_VERSION)
+		ZSS_VERSION = officialVersionRegExpr.search(zssBundleFileName).group(0)
+		print("zss version: "+ZSS_VERSION)
+		
 
 # upload to potix release server
 def uploadMavenBundle():
-	for projectName in zssList+zpoiList:
+	for projectName in ZSS_PROJECT_LIST+ZPOI_PROJECT_LIST:
 		bundleFileName = projectName+"-"+getProjectVersion(projectName)+"-bundle.jar"
 		shutil.copyfile(getBundleFilePath(projectName)+bundleFileName, getBundleFileTargetFolder(projectName)+"/"+bundleFileName)
 		print("copied "+bundleFileName )
-		
+	#copy proprietary bundle files 
+	global isFreshlyVersion
+	if (not isFreshlyVersion):
+		for projectName in ZSS_PROJECT_LIST+ZPOI_PROJECT_LIST:
+			bundleFileName = projectName+"-"+getProjectVersion(projectName)+"-bundle.jar"
+			shutil.copyfile(getProprietaryFilePath(projectName)+bundleFileName, getProprietaryFileTargetFolder(projectName)+"/"+bundleFileName)
+			print("copied "+bundleFileName )
 		
 def createArtifactFolder():
-	for project in zssList+zpoiList:
+	for project in ZSS_PROJECT_LIST+ZPOI_PROJECT_LIST:
 		createFolderIfNotExist(getBundleFileTargetFolder(project))
+	global isFreshlyVersion
+	if (not isFreshlyVersion):
+		for project in ZSS_PROJECT_LIST+ZPOI_PROJECT_LIST:
+			createFolderIfNotExist(getProprietaryFileTargetFolder(project))
 			
 def createFolderIfNotExist(path):
 	if (not os.path.exists(path)):
@@ -72,18 +97,31 @@ def createFolderIfNotExist(path):
 def getBundleFileTargetFolder(projectName):
 	return MOUNTED_RELEASE_PATH+projectName+'/releases/'+getProjectVersion(projectName)+'/maven'
 
+def getProprietaryFileTargetFolder(projectName):
+	return MOUNTED_RELEASE_PATH+projectName+'/releases/'+getProjectVersion(projectName)+'/maven/proprietary'
+
 def getProjectVersion(projectName):
-	if projectName in zssList:
+	if projectName in ZSS_PROJECT_LIST:
 		return ZSS_VERSION
-	elif projectName in zpoiList:
+	elif projectName in ZPOI_PROJECT_LIST:
 		return ZPOI_VERSION
 
 def getBundleFilePath(projectName):
-	if projectName in zssList:
-		return LOCAL_RELEASE_PATH+ZSS_BUNDLE_FILE_PATH
-	elif projectName in zpoiList:
-		return LOCAL_RELEASE_PATH+ZPOI_BUNDLE_FILE_PATH
+	global isFreshlyVersion
+	if (isFreshlyVersion):
+		bundleFilePath = 'FL/'
+	else:
+		bundleFilePath = 'eval/'
+	if projectName in ZSS_PROJECT_LIST:
+		return LOCAL_RELEASE_PATH+ZSS_MAVEN_PATH+bundleFilePath
+	elif projectName in ZPOI_PROJECT_LIST:
+		return LOCAL_RELEASE_PATH+ZPOI_MAVEN_PATH+bundleFilePath
 
+def getProprietaryFilePath(projectName):
+	if projectName in ZSS_PROJECT_LIST:
+		return LOCAL_RELEASE_PATH+ZSS_MAVEN_PATH+'proprietary/'
+	elif projectName in ZPOI_PROJECT_LIST:
+		return LOCAL_RELEASE_PATH+ZPOI_MAVEN_PATH+'proprietary/'
 
 initProjectVersion()
 mountRemoteFolder()
