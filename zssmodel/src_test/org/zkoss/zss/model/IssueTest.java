9fe3a8a46e6bcabcf5f40e0e070539db250345a9
Dennis Chen <atticcat@gmail.com>
2014-03-06 17:01:40 +0800

    fix ZSS-582 Can't display chart after rename the sheet name to dot(.)

--- zssmodel/src_test/org/zkoss/zss/model/IssueTest.java
+++ zssmodel/src_test/org/zkoss/zss/model/IssueTest.java
@@ -1,69 +1,20 @@
 package org.zkoss.zss.model;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 import java.util.ArrayList;
-import java.util.Date;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Locale;
-import java.util.Set;
-import java.util.concurrent.locks.ReadWriteLock;
 
 import junit.framework.Assert;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.zkoss.poi.ss.util.CellReference;
 import org.zkoss.util.Locales;
-import org.zkoss.zss.model.CellRegion;
-import org.zkoss.zss.model.ErrorValue;
-import org.zkoss.zss.model.InvalidateModelOpException;
-import org.zkoss.zss.model.SAutoFilter;
-import org.zkoss.zss.model.SBook;
-import org.zkoss.zss.model.SBooks;
-import org.zkoss.zss.model.SCell;
-import org.zkoss.zss.model.SCellStyle;
-import org.zkoss.zss.model.SChart;
-import org.zkoss.zss.model.SColumn;
-import org.zkoss.zss.model.SColumnArray;
-import org.zkoss.zss.model.SDataValidation;
-import org.zkoss.zss.model.SFont;
-import org.zkoss.zss.model.SName;
-import org.zkoss.zss.model.SPicture;
-import org.zkoss.zss.model.SRow;
-import org.zkoss.zss.model.SSheet;
-import org.zkoss.zss.model.ViewAnchor;
-import org.zkoss.zss.model.SheetRegion;
-import org.zkoss.zss.model.SAutoFilter.FilterOp;
-import org.zkoss.zss.model.SAutoFilter.NFilterColumn;
-import org.zkoss.zss.model.SCell.CellType;
-import org.zkoss.zss.model.SCellStyle.Alignment;
-import org.zkoss.zss.model.SCellStyle.BorderType;
-import org.zkoss.zss.model.SCellStyle.FillPattern;
-import org.zkoss.zss.model.SChart.ChartType;
-import org.zkoss.zss.model.SDataValidation.ValidationType;
-import org.zkoss.zss.model.SFont.Boldweight;
-import org.zkoss.zss.model.SFont.TypeOffset;
-import org.zkoss.zss.model.SFont.Underline;
-import org.zkoss.zss.model.SHyperlink.HyperlinkType;
-import org.zkoss.zss.model.SPicture.Format;
-import org.zkoss.zss.model.chart.SGeneralChartData;
-import org.zkoss.zss.model.chart.SSeries;
 import org.zkoss.zss.model.impl.AbstractBookSeriesAdv;
-import org.zkoss.zss.model.impl.AbstractCellAdv;
-import org.zkoss.zss.model.impl.AbstractSheetAdv;
-import org.zkoss.zss.model.impl.BookImpl;
 import org.zkoss.zss.model.impl.RefImpl;
 import org.zkoss.zss.model.impl.SheetImpl;
 import org.zkoss.zss.model.impl.sys.DependencyTableImpl;
 import org.zkoss.zss.model.sys.dependency.DependencyTable;
 import org.zkoss.zss.model.sys.dependency.Ref;
-import org.zkoss.zss.model.util.CellStyleMatcher;
-import org.zkoss.zss.model.util.FontMatcher;
-import org.zkoss.zss.model.util.Validations;
 import org.zkoss.zss.range.SRanges;
 
 public class IssueTest {
@@ -194,4 +145,21 @@ public class IssueTest {
 		Assert.assertEquals(0, ref.getLastColumn());		
 	}	
 	
+	@Test
+	public void testZSS582(){
+		SBook book = SBooks.createBook("book1");
+		book.getBookSeries().setAutoFormulaCacheClean(true);
+		
+		SSheet sheet1 = book.createSheet("Sheet1");
+		sheet1.getCell("A1").setValue(10D);
+		sheet1.getCell("A2").setValue("=Sheet1!A1");
+		Assert.assertEquals(10D, sheet1.getCell("A2").getValue());
+		
+		SRanges.range(sheet1).setSheetName(".ABC");
+		Assert.assertEquals("'.ABC'!A1", sheet1.getCell("A2").getFormulaValue());
+		
+		sheet1.getCell("A1").setValue(20D);
+		Assert.assertEquals(20D, sheet1.getCell("A2").getValue());
+		
+	}
 }
