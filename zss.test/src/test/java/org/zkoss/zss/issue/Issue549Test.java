33b383bfc15385ec562f53b8f10b817f3d0509e0
Henri Chen <henrichen@zkoss.org>
2014-09-17 19:00:40 +0800

    ZSS-549 Enlarge a text to exceed its row height, the text will not be exported to PDF

---
+++ zss.test/src/test/java/org/zkoss/zss/issue/Issue549Test.java
@@ -0,0 +1,89 @@
+package org.zkoss.zss.issue;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Locale;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.zkoss.zss.Setup;
+import org.zkoss.zss.Util;
+import org.zkoss.zss.api.model.Book;
+import org.zkoss.zss.model.SBook;
+import org.zkoss.zss.model.SBooks;
+import org.zkoss.zss.model.SCell;
+import org.zkoss.zss.model.SFont;
+import org.zkoss.zss.model.SRichText;
+import org.zkoss.zss.model.SSheet;
+import org.zkoss.zss.model.SFont.Boldweight;
+import org.zkoss.zss.model.SFont.Underline;
+import org.zkoss.zss.model.impl.pdf.PdfExporter;
+import org.zkoss.zss.range.SImporter;
+import org.zkoss.zss.range.impl.imexp.ExcelImportFactory;
+
+public class Issue549Test {
+	
+	@BeforeClass
+	public static void setUpLibrary() throws Exception {
+		Setup.touch();
+	}
+	
+	@Before
+	public void startUp() throws Exception {
+		Setup.pushZssLocale(Locale.TAIWAN);
+	}
+	
+	@After
+	public void tearDown() throws Exception {
+		Setup.popZssLocale();
+	}
+
+	@Test
+	public void exportLargeTextA1WrapGridBottom() {
+		Book book = Util.loadBook(this, "book/549-large-text-wrap-grid-bottom-pdf.xlsx");
+		
+		File temp = Setup.getTempFile("Issue549WrapGridBottomTest",".pdf");
+		
+		exportBook(book.getInternalBook(), temp);
+		
+		Util.open(temp);
+	}
+
+	@Test
+	public void exportLargeTextA1WrapGridMiddle() {
+		Book book = Util.loadBook(this, "book/549-large-text-wrap-grid-middle-pdf.xlsx");
+		
+		File temp = Setup.getTempFile("Issue549WrapGridMiddleTest",".pdf");
+		
+		exportBook(book.getInternalBook(), temp);
+		
+		Util.open(temp);
+	}
+	
+	@Test
+	public void exportLargeTextA1WrapGridTop() {
+		Book book = Util.loadBook(this, "book/549-large-text-wrap-grid-top-pdf.xlsx");
+		
+		File temp = Setup.getTempFile("Issue549WrapGridTopTest",".pdf");
+		
+		exportBook(book.getInternalBook(), temp);
+		
+		Util.open(temp);
+	}
+	
+	private void exportBook(SBook book, File file) {
+		
+		PdfExporter exporter = new PdfExporter();
+		try {
+			exporter.export(book, file);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
