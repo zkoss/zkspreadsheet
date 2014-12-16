aebe62d6bdfa4ba7fce1d51cc5105605a4e4b5a9
Henri Chen <henrichen@zkoss.org>
2014-12-16 18:36:12 +0800

    ZSS-852 Support condition in SUMPRODUCT function

---
+++ zss.test/src/test/java/org/zkoss/zss/issue/Issue852Test.java
@@ -0,0 +1,51 @@
+package org.zkoss.zss.issue;
+
+import java.io.IOException;
+import java.util.Locale;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.zkoss.zss.Setup;
+import org.zkoss.zss.Util;
+import org.zkoss.zss.api.Range;
+import org.zkoss.zss.api.Ranges;
+import org.zkoss.zss.api.model.Book;
+import org.zkoss.zss.api.model.Sheet;
+
+public class Issue852Test {
+	
+	@BeforeClass
+	public static void setUpLibrary() throws Exception {
+		Setup.touch();
+	}
+	
+	@Before
+	public void startUp() throws Exception {
+		Setup.pushZssLocale(Locale.TAIWAN);
+	}
+	
+	@After
+	public void tearDown() throws Exception {
+		Setup.popZssLocale();
+	}
+
+	/**
+	 * parse a defined name which contains question mark '?'
+	 * @throws IOException 
+	 */
+	@Test
+	public void conditionalSumproduct() throws IOException {
+		Book book = Util.loadBook(this, "book/852-conditional-sumproduct.xlsx");
+		Sheet sheet1 = book.getSheetAt(0);
+		Range rng = Ranges.range(sheet1, "D1");
+		String text = rng.getCellFormatText();
+		Assert.assertEquals("=SUMPRODUCT(--(A1:A3=\"John\"),(B1:B3),(C1:C3))", "10", text);
+		
+		rng = Ranges.range(sheet1, "I1");
+		text = rng.getCellFormatText();
+		Assert.assertEquals("=SUMPRODUCT((F12:F21=1)*(G12:G21=\"Z\")*H12:H21)", "11", text);
+	}
+}
