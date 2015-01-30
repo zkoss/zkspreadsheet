8debf839aa5923a26faae1ba4658541808689b59
jerrywell <b80203@gmail.com>
2015-01-30 05:59:05 +0000

    update version to 3.8.0

--- zss/src/org/zkoss/zss/ui/Version.java
+++ zss/src/org/zkoss/zss/ui/Version.java
@@ -29,7 +29,7 @@ import org.zkoss.lang.Classes;
 public class Version {
 	/** Returns the version UID.
 	 */
-	public static final String UID = "3.7.0";
+	public static final String UID = "3.8.0";
 	
 	/** Features. */
 	private static final boolean
