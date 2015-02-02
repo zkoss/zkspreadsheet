930d5a9741bc96bcb6ac7a5e718b1bd443790cd7
Henri Chen <henrichen@zkoss.org>
2015-02-02 17:09:04 +0800

    ZSS-914 java.lang.IndexOutOfBoundsException on HSSFRow.getRowStyle()

---
+++ zss.test/src/test/java/org/zkoss/zss/issue/Issue914Test.java
@@ -0,0 +1,54 @@
+package org.zkoss.zss.issue;
+
+import java.util.Locale;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.zkoss.zss.Setup;
+import org.zkoss.zss.Util;
+import org.zkoss.zss.api.Range;
+import org.zkoss.zss.api.Ranges;
+import org.zkoss.zss.api.model.Book;
+import org.zkoss.zss.api.model.CellData;
+import org.zkoss.zss.api.model.CellData.CellType;
+import org.zkoss.zss.api.model.Sheet;
+import org.zkoss.zss.model.SBook;
+import org.zkoss.zss.model.SBooks;
+import org.zkoss.zss.model.SSheet;
+import org.zkoss.zss.range.SRange;
+import org.zkoss.zss.range.SRanges;
+
+public class Issue914Test {
+	
+	@BeforeClass
+	public static void setUpLibrary() throws Exception {
+		Setup.touch();
+	}
+	
+	@Before
+	public void startUp() throws Exception {
+		Setup.pushZssLocale(Locale.TAIWAN);
+	}
+	
+	@After
+	public void tearDown() throws Exception {
+		Setup.popZssLocale();
+	}
+
+	/**
+	 * Test a cell with text format should see =xxx a string rather than a 
+	 * formula
+	 */
+	@Test
+	public void testLoadRowStyleWithDoubleBorder() {
+		try  {
+			Book book = Util.loadBook(this, "book/914-row-style.xls");
+			Assert.assertTrue("No Exception", true);
+		} catch (Exception e) {
+			Assert.assertTrue("Exception when load \"issue/book/914-row-style.xls\":\n" + e, false);
+		}
+	}
+}
