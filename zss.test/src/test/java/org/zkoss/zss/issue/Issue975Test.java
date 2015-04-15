dce4d17c50019d929645efa72472c89682a094d0
Henri Chen <henrichen@zkoss.org>
2015-04-15 18:46:14 +0800

    ZSS-975 Export bold text to PDF file

---
+++ zss.test/src/test/java/org/zkoss/zss/issue/Issue975Test.java
@@ -0,0 +1,60 @@
+package org.zkoss.zss.issue;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Locale;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.zkoss.zss.Setup;
+import org.zkoss.zss.Util;
+import org.zkoss.zss.api.model.Book;
+import org.zkoss.zss.model.SBook;
+import org.zkoss.zss.model.impl.pdf.PdfExporter;
+
+public class Issue975Test {
+	
+	@BeforeClass
+	public static void setUpLibrary() throws Exception {
+		Setup.touch();
+	}
+	
+	@Before
+	public void startUp() throws Exception {
+		Setup.pushZssLocale(Locale.TAIWAN);
+	}
+	
+	@After
+	public void tearDown() throws Exception {
+		Setup.popZssLocale();
+	}
+
+	/**
+	 * Test export to pdf with hidden row and column.
+	 */
+	@Test
+	public void exportFillPatterns() {
+		Book book = Util.loadBook(this, "book/975-bold-text-pdf.xlsx");
+		
+		File temp = Setup.getTempFile("Issue975BoldTextTest",".pdf");
+		
+		exportBook(book.getInternalBook(), temp);
+		
+		Util.open(temp);
+	}
+	
+	private void exportBook(SBook book, File file) {
+		
+		PdfExporter pdfExporter = new PdfExporter();
+		
+		try {
+			pdfExporter.export(book, file);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
