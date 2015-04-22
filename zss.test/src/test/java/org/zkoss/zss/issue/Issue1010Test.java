668eaeba399bc76222378900d121654c70bf198f
Henri Chen <henrichen@zkoss.org>
2015-04-22 14:15:57 +0800

    ZSS-1010 IndexOutOfBoundsException when export the file

---
+++ zss.test/src/test/java/org/zkoss/zss/issue/Issue1010Test.java
@@ -0,0 +1,62 @@
+package org.zkoss.zss.issue;
+
+import java.util.Locale;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.zkoss.zss.Setup;
+import org.zkoss.zss.Util;
+import org.zkoss.zss.api.Range;
+import org.zkoss.zss.api.Ranges;
+import org.zkoss.zss.api.model.Book;
+import org.zkoss.zss.api.model.CellData;
+import org.zkoss.zss.api.model.CellData.CellType;
+import org.zkoss.zss.api.model.Sheet;
+import org.zkoss.zss.model.SBook;
+import org.zkoss.zss.model.SBooks;
+import org.zkoss.zss.model.SSheet;
+import org.zkoss.zss.range.SRange;
+import org.zkoss.zss.range.SRanges;
+import org.zkoss.zss.api.Exporters;
+
+public class Issue1010Test {
+	
+	@BeforeClass
+	public static void setUpLibrary() throws Exception {
+		Setup.touch();
+	}
+	
+	@Before
+	public void startUp() throws Exception {
+		Setup.pushZssLocale(Locale.TAIWAN);
+	}
+	
+	@After
+	public void tearDown() throws Exception {
+		Setup.popZssLocale();
+	}
+
+	/**
+	 * Test a book with print area and export it and it should not throw exception
+	 * @throws IOException 
+	 */
+	@Test
+	public void testExportXlsBookWithPrintArea() throws IOException {
+		ByteArrayOutputStream os = new ByteArrayOutputStream();
+		try  {
+			Book book = Util.loadBook(this, "book/1010-export-IOOBE.xlsx");
+			Assert.assertTrue("No Exception", true);
+			Exporters.getExporter("xlsx").export(book, os);
+//			Filedownload.save(os.toByteArray(), "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "scale0.xlsx"); 
+		} catch (Exception e) {
+			Assert.assertTrue("Exception when load \"issue/book/1010-export-IOOBE.xls\":\n" + e, false);
+		} finally {
+			 os.close();
+		}
+	}
+}
