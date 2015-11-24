adb2dbff9976187d729e5c9d54abfd818016f259
Jerry Chen <b80203@gmail.com>
2015-11-24 18:08:36 +0800

    update version to 3.8.3 for zss, 3.9.10 for zpoi

--- zss/src/org/zkoss/zss/ui/Version.java
+++ zss/src/org/zkoss/zss/ui/Version.java
@@ -29,7 +29,7 @@ import org.zkoss.lang.Classes;
 public class Version {
 	/** Returns the version UID.
 	 */
-	public static final String UID = "3.8.2";
+	public static final String UID = "3.8.3";
 	
 	/** Features. */
 	private static final boolean
