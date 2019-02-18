192f8042a3e6239e67263779d52e3f0d90c88986
hawkchen <hawkchen@potix.com>
2019-02-18 17:36:27 +0800

    update maven build script for parent pom

--- bin/mavenBuild.sh
+++ bin/mavenBuild.sh
@@ -5,8 +5,11 @@
 bundleGoals='clean source:jar javadoc:jar repository:bundle-create -Dmaven.test.skip=true'
 # a relative path based on the current path
 zpoiPom='zsspoi/zpoi/pom.xml'
+zssParent='zkspreadsheet/zssparent/'
 zssmodelPom='zkspreadsheet/zssmodel/'
 zssPom='zkspreadsheet/zss/'
+
+zsscmlParent='zsscml/zsscmlparent/'
 zpoiexPom='zsscml/zpoiex/'
 zssexPom='zsscml/zssex/'
 zssjsfPom='zsscml/zssjsf/'
@@ -26,12 +29,14 @@ function buildBundleInstall(){
 # build a maven bundle file
 function buildBundle(){
     mvn -f $1 versions:set -DremoveSnapshot #remove '-SNAPSHOT' from project version
-    if [[ $2 != "official" ]]
+    if [[ $2 = "official" ]]
     then
+        mvn -B -f $1 -P official ${bundleGoals}
+    else
         mvn -f $1 -P build-fl validate # set freshly version
+        # http://maven.apache.org/plugins/maven-repository-plugin/usage.html
+        mvn -B -f $1 ${bundleGoals}
     fi
-    # http://maven.apache.org/plugins/maven-repository-plugin/usage.html
-    mvn -B -f $1 ${bundleGoals}
 }
 
 
@@ -55,10 +60,14 @@ buildBundleInstall ${zpoiPom} $1
 zpoiVersion=$(python zkspreadsheet/bin/read_pom_version.py ${zpoiPom})
 echo zpoi version: ${zpoiVersion}
 
+mvn -f ${zssParent} install # install parent pom so that child project can resolve during building
 # versions:use-latest-releases, version:update-property both failed set zpoi version in Jenkins
 setZpoiVersion ${zssmodelPom}
 buildBundleInstall ${zssmodelPom} $1
 buildBundleInstall ${zssPom} $1
+
+
+mvn -f ${zsscmlParent} install # install parent pom so that child project can resolve during building
 buildBundleInstall ${zpoiexPom} $1
 setZpoiVersion ${zssexPom}
 buildBundleInstall ${zssexPom} $1
