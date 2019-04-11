32cf8b533b5f6082abe357b1a617fbd2d0dd8c27
hawkchen <hawkchen@potix.com>
2019-04-11 16:49:34 +0800

    refine fill million cells test

--- zss.test/src/main/java/zss/performance/Fill1MillionComposer.java
+++ zss.test/src/main/java/zss/performance/Fill1MillionComposer.java
@@ -11,6 +11,7 @@ Copyright (C) 2010 Potix Corporation. All Rights Reserved.
  */
 package zss.performance;
 
+import org.zkoss.zk.ui.Component;
 import org.zkoss.zk.ui.select.SelectorComposer;
 import org.zkoss.zk.ui.select.annotation.*;
 import org.zkoss.zss.api.*;
@@ -24,10 +25,14 @@ public class Fill1MillionComposer extends SelectorComposer {
     @Wire
     Spreadsheet ss;
 
+    @Override
+    public void doAfterCompose(Component comp) throws Exception {
+        super.doAfterCompose(comp);
+        ss.focusTo(1000, 1000);
+    }
 
     @Listen("onCellDoubleClick = #ss")
     public void onCellDoubleClick(CellMouseEvent event) {
-        Ranges.range(ss.getSelectedSheet(), 0, 0, 1000, 1000).notifyChange();
         for (int row = 0; row < 1000; row++) {
             for (int column = 0; column < 1000; column++) {
                 Range range = Ranges.range(ss.getSelectedSheet(), row, column);
