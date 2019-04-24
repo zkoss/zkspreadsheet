9852efce38348af7909531c1cc78767972f7d868
hawkchen <hawkchen@potix.com>
2019-04-24 16:27:36 +0800

    refactor

--- zss/src/org/zkoss/zss/ui/impl/ua/PasteHandler.java
+++ zss/src/org/zkoss/zss/ui/impl/ua/PasteHandler.java
@@ -16,21 +16,14 @@ Copyright (C) 2013 Potix Corporation. All Rights Reserved.
 */
 package org.zkoss.zss.ui.impl.ua;
 
-import org.zkoss.poi.ss.SpreadsheetVersion;
 import org.zkoss.util.resource.Labels;
-import org.zkoss.zss.api.AreaRefWithType;
-import org.zkoss.zss.api.Range;
-import org.zkoss.zss.api.Ranges;
-import org.zkoss.zss.api.AreaRef;
+import org.zkoss.zss.api.*;
 import org.zkoss.zss.api.impl.RangeImpl;
-import org.zkoss.zss.api.model.Book;
-import org.zkoss.zss.api.model.Sheet;
+import org.zkoss.zss.api.model.*;
 import org.zkoss.zss.range.impl.PasteRangeImpl;
-import org.zkoss.zss.ui.CellSelectionType;
-import org.zkoss.zss.ui.UserActionContext;
+import org.zkoss.zss.ui.*;
 import org.zkoss.zss.ui.UserActionContext.Clipboard;
-import org.zkoss.zss.ui.impl.undo.CutCellAction;
-import org.zkoss.zss.ui.impl.undo.PasteCellAction;
+import org.zkoss.zss.ui.impl.undo.*;
 import org.zkoss.zss.ui.sys.UndoableActionManager;
 
 public class PasteHandler extends AbstractHandler {
@@ -44,7 +37,6 @@ public class PasteHandler extends AbstractHandler {
 			return false;
 		}
 		
-		Book book = ctx.getBook();
 		Sheet destSheet = ctx.getSheet();
 		Sheet srcSheet = cb.getSheet();
 		if(srcSheet==null){
@@ -53,12 +45,11 @@ public class PasteHandler extends AbstractHandler {
 			return true;
 		}
 		//ZSS-717
-		// 20160125, henrichen: The spec is we copy only those values within 
-		// MaxVisible range so we save some time 
+		// 20160125, henrichen: The spec is we copy only those values within MaxVisible range to save some time
 		AreaRefWithType src = cb.getSelectionWithType();
-		AreaRefWithType selection = ctx.getSelectionWithType();
+		AreaRefWithType dst = ctx.getSelectionWithType();
 		CellSelectionType srcType = src.getSelType();
-		CellSelectionType dstType = selection.getSelType();
+		CellSelectionType dstType = dst.getSelType();
 		final boolean srcWholeRow = srcType == CellSelectionType.ALL || srcType == CellSelectionType.ROW;
 		final boolean srcWholeColumn = srcType == CellSelectionType.ALL || srcType == CellSelectionType.COLUMN;
 		final boolean dstWholeRow = dstType == CellSelectionType.ALL || dstType == CellSelectionType.ROW;
@@ -69,9 +60,9 @@ public class PasteHandler extends AbstractHandler {
 		int srcLastCol = srcWholeRow ?
 				Math.max(cb.getSheetMaxVisibleColumns() - 1, src.getLastColumn()) : src.getLastColumn();
 		int dstLastRow = dstWholeColumn ?
-				Math.max(ctx.getSheetMaxVisibleRows() - 1, selection.getLastRow()) : selection.getLastRow();
+				Math.max(ctx.getSheetMaxVisibleRows() - 1, dst.getLastRow()) : dst.getLastRow();
 		int dstLastCol = dstWholeRow ?
-				Math.max(ctx.getSheetMaxVisibleColumns() - 1,  selection.getLastColumn()) : selection.getLastColumn();
+				Math.max(ctx.getSheetMaxVisibleColumns() - 1,  dst.getLastColumn()) : dst.getLastColumn();
 				
 		if (srcWholeColumn && dstWholeColumn && srcLastRow != dstLastRow) {
 			dstLastRow = srcLastRow = Math.max(dstLastRow, srcLastRow);
@@ -83,8 +74,8 @@ public class PasteHandler extends AbstractHandler {
 		Range srcRange = new RangeImpl(new PasteRangeImpl(srcSheet.getInternalSheet(), src.getRow(),
 				src.getColumn(), srcLastRow,srcLastCol, srcWholeRow, srcWholeColumn), srcSheet);
 
-		Range destRange = new RangeImpl(new PasteRangeImpl(destSheet.getInternalSheet(), selection.getRow(),
-				selection.getColumn(), dstLastRow, dstLastCol, dstWholeRow, dstWholeColumn), destSheet);
+		Range destRange = new RangeImpl(new PasteRangeImpl(destSheet.getInternalSheet(), dst.getRow(),
+				dst.getColumn(), dstLastRow, dstLastCol, dstWholeRow, dstWholeColumn), destSheet);
 		
 		if (destRange.isProtected()) {
 			showProtectMessage();
@@ -106,7 +97,7 @@ public class PasteHandler extends AbstractHandler {
 				srcSheet, srcRange.getRow(), srcRange.getColumn(),srcRange.getLastRow(), srcRange.getLastColumn(),srcRange.isWholeColumn(),srcRange.isWholeRow(), //ZSS-1277
 				destSheet, destRange.getRow(), destRange.getColumn(),destRange.getLastRow(), destRange.getLastColumn(),destRange.isWholeColumn(),destRange.isWholeRow())); //ZSS-1277
 			// ZSS-1380 clear highlight when pasting range overlaps the source range
-			if (srcSheet.equals(destSheet) && src.overlap(selection)){
+			if (srcSheet.equals(destSheet) && src.overlap(dst)){
 				ctx.clearClipboard();
 			}
 
